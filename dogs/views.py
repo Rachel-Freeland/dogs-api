51592d9541ced6397ae64ff198249bb64da9e6f6
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-31 01:05:37 -0500

    Finished the REST framework

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Dog
+from .serializers import DogSerializer
 
-# Create your views here.
+
+class DogList(generics.ListCreateAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+
+class DogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
